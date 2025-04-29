c1581dd6d65880030080bdcc89c853f1c5a98b13
Hasan0734 <jahidhasan2856@gmail.com>
2025-04-29 19:25:16 +0600

    adding board task list

--- components/board/AddNewList.tsx
+++ components/board/AddNewList.tsx
@@ -3,45 +3,53 @@ import { Card, CardContent } from "../ui/card";
 import { Button } from "../ui/button";
 import { PlusIcon, XIcon } from "lucide-react";
 import { Input } from "../ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 const AddNewList = () => {
-  const [addNew, setAddNew] = useState(false);
-  const [listName, setListName] = useState('')
+  const [modalOpen, setModalOpen] = useState(false);
+  const [listName, setListName] = useState("");
 
+  const handlePopover = (open: boolean) => {
+    setModalOpen(open);
+  };
+
+  const hanldClose = () => {
+    setModalOpen(false);
+  };
   return (
-    <div className="max-w-[272px]">
-      {!addNew && (
-        <Button
-          onClick={() => setAddNew(true)}
-          variant={"secondary"}
-          className="w-full justify-start cursor-pointer"
-        >
-          <PlusIcon /> Add a list
-        </Button>
-      )}
-      {addNew && (
-        <Card className="bg-primary py-0">
-          <CardContent className="p-2 space-y-2">
-            <Input onChange={(e) => setListName(e.target.value)} placeholder="Enter list name..." />
-            <div className="flex items-center gap-1">
-              <Button
-                className="bg-blue-500 hover:bg-blue-600 cursor-pointer"
-                variant={"default"}
-              >
-                Add list
-              </Button>
-              <Button
-                onClick={() => setAddNew(false)}
-                variant={"outline"}
-                size={"icon"}
-              >
-                <XIcon className="" />
-              </Button>
-            </div>
-          </CardContent>
-        </Card>
-      )}
-    </div>
+    <Popover open={modalOpen} onOpenChange={handlePopover}>
+      <div className="max-w-[272px]">
+        <PopoverTrigger className="w-full" asChild>
+          <Button
+            variant={"secondary"}
+            className="w-full justify-start cursor-pointer"
+          >
+            <PlusIcon /> Add a list
+          </Button>
+        </PopoverTrigger>
+      </div>
+
+      <PopoverContent
+        sideOffset={-36}
+        className="w-[272px] p-2 bg-primary space-y-3 border-0 -top-10"
+      >
+        <Input
+          onChange={(e) => setListName(e.target.value)}
+          placeholder="Enter list name..."
+        />
+        <div className="flex items-center gap-1">
+          <Button
+            className="bg-blue-500 hover:bg-blue-600 cursor-pointer"
+            variant={"default"}
+          >
+            Add list
+          </Button>
+          <Button onClick={hanldClose} variant={"outline"} size={"icon"}>
+            <XIcon className="" />
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 };
 
--- components/board/BoardDetails.tsx
+++ components/board/BoardDetails.tsx
@@ -1,14 +1,18 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 import AddNewList from "./AddNewList";
 import { Card } from "../ui/card";
 
 const BoardDetails = () => {
+  const [items, setItems] = useState<string[]>([]);
+
+  const handleAddItem = (newItem: string) => {
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
 
   return (
     <Card className="bg-orange-500 h-dvh p-4">
-      
       <AddNewList/>
     </Card>
   );
